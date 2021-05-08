6fd03f10eae7eeb87f86e07f2c7835e33bc796fa
Zeel <zeelp741@gmail.com>
2021-05-08 02:45:10 -0400

    completed class , objects

--- HandTracking.py
+++ HandTracking.py
@@ -1,74 +1,130 @@
-import cv2 
-import mediapipe as mp 
+import cv2
+import mediapipe as mp
 import time
 import pandas as pd
 import keyboard
 
-capture = cv2.VideoCapture(0)
 
-# Global Varibles
-prevTime = 0
-currTime = 0
+class HandDetector():
+    def __init__(self, mode = False, maxHands = 2, detectionConfidence = 0.5, trackConfidence = 0.5):
+        # Default Values
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionConfidence
+        self.trackCon = trackConfidence
 
-# Webcam Dimensions
-width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # Draws Hand
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
-# Checks possible webcams
-url = "https://en.wikipedia.org/wiki/List_of_common_resolutions"
-table = pd.read_html(url)[0]
-table.columns = table.columns.droplevel()
+    def locateHands(self, img, draw = True):
 
-resolutions = {}
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+            
+        # Draws Landmarkers on Hands if detected
+        if self.results.multi_hand_landmarks:
+            for handLandMarks in self.results.multi_hand_landmarks:
+                if draw == True:
+                    self.mpDraw.draw_landmarks(img, handLandMarks, self.mpHands.HAND_CONNECTIONS)
 
-for index, row in table[["W", "H"]].iterrows():
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, row["W"])
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, row["H"])
-    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    resolutions[str(width)+" x "+str(height)] = "OK"
+        return img
 
+    def locatePosition(self, img, handNum = 0, draw = True):
+        landMarksList = []
 
-mpHands = mp.solutions.hands            # static_image-Mode = False, max_nim_hands = 2, min_detection_confidence = 0.5, min_tracking_confidence = 2
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+        if self.results.multi_hand_landmarks:
+            hands = self.results.multi_hand_landmarks[handNum]
 
+            for id, landMarks in enumerate(hands.landmark):
+                # print(id, landMarks)
+                height, width, channels = img.shape
+                cx, cy = int(landMarks.x * width), int(landMarks.y * height)
+                # print(id, cx, cy)
 
-def fps(prevTime, currTime):
+                landMarksList.append([id, cx, cy])
+
+                if draw == True and id == 8:
+                    cv2.circle(img, (cx, cy), 15, (255, 255, 255), cv2.FILLED)
+        
+        return landMarksList
+
+def fps(img, prevTime, currTime, WIDTH, HEIGHT):
     # Calculates the FPS
     currTime = time.time()
     fps  = 1 / (currTime - prevTime)
     prevTime = currTime
-    cv2.putText(img, "FPS: " + str(int(fps)), (int(width - 200) , 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
+    cv2.putText(img, "FPS: " + str(int(fps)), (int(WIDTH - 200) , 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
 
-    cv2.putText(img, "Res: " + str(list(resolutions)[-1]), (int(width - 520) , 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
+    return prevTime, currTime  
 
-    return prevTime, currTime
+def webcamResolution(capture):
+    # Checks possible webcams
+    url = "https://en.wikipedia.org/wiki/List_of_common_resolutions"
+    table = pd.read_html(url)[0]
+    table.columns = table.columns.droplevel()
 
-while True:
+    resolutions = {}
 
-    success, img = capture.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+    for index, row in table[["W", "H"]].iterrows():
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, row["W"])
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, row["H"])
+        width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        resolutions[str(width)+" x "+str(height)] = "OK"
 
-    prevTime, currTime = fps(prevTime, currTime)
+    print(resolutions)
+    
+    return resolutions
 
-    # Detects Hand
-    # print(results.multi_hand_landmarks)
+def displayResolution(img, resolutions, WIDTH, HEIGHT):
+    cv2.putText(img, "Res: " + str(list(resolutions)[-1]), (int(WIDTH - 520) , 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
 
-    # Draws Landmarkers on Hands if detected
-    if results.multi_hand_landmarks:
-        for handLandMarks in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLandMarks, mpHands.HAND_CONNECTIONS)
 
-    # Exits program 
-    try: 
-        if keyboard.is_pressed('q'):  
-            break 
-    except:
-        break  
+def main():
 
-    cv2.imshow("Webcam", img)
-    if cv2.waitKey(1) and 0xFF == ord('q'):
-        break
+    #  Varibles
+    prevTime = 0
+    currTime = 0
 
+    capture = cv2.VideoCapture(0)
+    hand_detector = HandDetector()
+
+    # Webcam Dimensions
+    WIDTH = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    HEIGHT = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    resolutions = webcamResolution(capture)
+    print(resolutions)
+
+    while True:
+
+        WIDTH = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+        HEIGHT = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        success, img = capture.read()
+
+        img = hand_detector.locateHands(img)
+        landMarkList  = hand_detector.locatePosition(img)
+
+        if len(landMarkList ) != 0:
+            pass
+            # print(lmList[4])
+
+        prevTime, currTime = fps(img, prevTime, currTime, WIDTH, HEIGHT)
+        displayResolution(img, resolutions, WIDTH, HEIGHT)
+
+        try: 
+            if keyboard.is_pressed('q'):  
+                print("Program executed")
+                break 
+        except:
+            break  
+
+        cv2.imshow("Webcam", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
